08c641f8e3545b3ab2831ec1808a7cdb8723f79e
Kaleb Tavares <148735188+kalebtavares@users.noreply.github.com>
2025-04-07 18:55:34 -0400

    Primeiro Comit projeto de processo seletivo.

---
+++ src/ProcessoSeletivo.java
@@ -0,0 +1,23 @@
+public class ProcessoSeletivo {
+    public static void  main(String[] args) {
+    analisandoCandidato(1900.0);
+    analisandoCandidato(2200.0);
+    analisandoCandidato(2000.0);
+
+
+    }
+    static void  analisandoCandidato(double salarioPretendido) {
+        double salarioBase = 2000.0;
+        if (salarioBase > salarioPretendido) {
+            System.out.println("LIGAR PARA CANDIDATO");
+
+        } else if (salarioBase == salarioPretendido)
+            System.out.println("LIGAR PARA CANDIDATO COM CONTRA PROPOSTA");
+        else {
+            System.out.println("AGUARDANDO RESULTADO DOS DEMAIS CANDIDATOS");
+
+        }
+
+    }
+}
+
