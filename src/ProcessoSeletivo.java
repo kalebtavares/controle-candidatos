c614708714e73ecf625458abb6424a80b5996007
Kaleb Tavares <148735188+kalebtavares@users.noreply.github.com>
2025-04-08 19:58:07 -0400

    mais um comit Processo Seletivo

--- src/ProcessoSeletivo.java
+++ src/ProcessoSeletivo.java
@@ -1,10 +1,32 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
     public static void  main(String[] args) {
-    analisandoCandidato(1950.0);
-    analisandoCandidato(2200.0);
-    analisandoCandidato(2000.0);
+        selecaoCandidatos();
 
+    }
+    static void selecaoCandidatos() {
+        String[] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE"};
 
+        int candidatosSelecionados = 0;
+        int candidatosAtual = 0;
+        double salarioBase = 2000.0;
+        while (candidatosSelecionados < 5 && candidatosAtual < candidatos.length) {
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O Candidato " + candidato + "Solicitou este valor de sálario " + salarioPretendido);
+            if(salarioBase >= salarioPretendido) {
+                System.out.println("O candidato " + candidato + "Foi selecionado para a vaga ");
+                candidatosSelecionados++;
+
+        }
+        candidatosAtual++;
+        }
+    }
+
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800,2200);
     }
     static void  analisandoCandidato(double salarioPretendido) {
         double salarioBase = 2000.0;
